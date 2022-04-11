43bf4da7e36242ef093846fd32d546dc4e4a33ca
midnightxd <leonardigit@gmail.com>
2022-04-11 10:34:34 -0300

    Update v1.0.0 20220411

--- Sessão 10/aula18/routes.js
+++ Sessão 10/aula18/routes.js
@@ -4,6 +4,7 @@ const homeController = require('./src/controller/homeController');
 const middleware = require('./src/middlewares/middleware')
 
 // Routes home
-routes.get('/', middleware, homeController.home);
+routes.get('/', homeController.home);
+routes.post('/', homeController.datePost);
 
 module.exports = routes;
--- Sessão 10/aula18/server.js
+++ Sessão 10/aula18/server.js
@@ -25,9 +25,10 @@ const helmet = require('helmet');
 const crsf = require('csurf');
 
 // App settings
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
